b59c504ac433f04091a6e40c28352c40899c4dfe
sunbaee._ <saculbertazo@gmail.com>
2024-07-04 23:51:07 -0300

    added collisions (need to add distance when square enters the ground) and dragging

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// Values
-
-function UpdateLabelValues() {
-    const slidersLis = document.querySelectorAll(".sliderLi");
-
-    slidersLis.forEach(element => {
-        var sliderValue = element.querySelector(".slider").value;
-        var sliderLabel = element.querySelector("label span");
-        sliderLabel.innerText = sliderValue;
-    });
-}
-
-// Physics
-
-const Sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-
 class Vector {
     constructor(x = 0, y = 0) {
         this.x = x;
@@ -28,83 +12,164 @@ class Vector {
         return Math.pow(this.x, 2) + Math.pow(this.y, 2);
     }
 
+    Add(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y);
+    }
+
     Subtract(vector) {
         return new Vector(this.x - vector.x, this.y - vector.y);
     }
 
+    Mult(value) {
+        return new Vector(this.x * value, this.y * value);
+    }
+
+    Normalized() {
+        if (this.x == 0 && this.y == 0) return new Vector();
+
+        const vec = new Vector(this.x, this.y);
+        return new Vector(this.x / vec.Mag(), this.y / vec.Mag());
+    }
 }
+
+// HTML config
+
+function UpdateLabelValues() {
+    const slidersLis = document.querySelectorAll(".sliderLi");
+
+    slidersLis.forEach(element => {
+        var sliderValue = element.querySelector(".slider").value;
+        var sliderLabel = element.querySelector("label span");
+        sliderLabel.innerText = sliderValue;
+    });
+}
+
+document.addEventListener("dragstart", e => {
+    e.preventDefault();
+});
+
+// Physics
+
+const Sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 const square = document.querySelector("#square");
 const container = document.querySelector(".container");
 
-const sqrColRadius = Math.round(window.innerHeight / 100 * 3);
+const startPosition = new Vector(innerWidth / 2, innerHeight * 1 / 4);
+const sqrPushRadius = Math.round(window.innerHeight / 100 * 6);
+const sqrHalfSize = Math.round(window.innerHeight / 100 * 3);
 
+const collisionDamping = 1; 
+const collisionFriction = 0.2;
+
+const gravity = 980; // value per frame ("cm")
+
+const velPerDist = 8;
+
+const VELOCITY_ERROR = 10;
+const MS_CONVERSOR = 0.001; // converts ms to seconds
+const FRAME_TIME = 16; // ms
+
+var sqrAcc = new Vector();
+var sqrVel = new Vector();
 var sqrPos = new Vector();
 
+var mPos = new Vector();
+
+var gPotVel;
 var groundY;
 var isPushing;
 
 window.onmousedown = e => {
-    var mPos = new Vector(e.clientX, e.clientY);
+    mPos = new Vector(e.clientX, e.clientY);
 
     const distVec = mPos.Subtract(sqrPos);
-    if (distVec.MagSqrd() <= Math.pow(sqrColRadius, 2)) isPushing = true;
+    if (distVec.MagSqrd() <= Math.pow(sqrPushRadius, 2)) isPushing = true;
 }
 
-window.onmouseup = e => {
+window.onmousemove = e => {
     if (!isPushing) return;
 
-    var direction = new Vector(e.clientX, e.clientY).Subtract(sqrPos);
-    console.log(direction);
+    mPos = new Vector(e.clientX, e.clientY);
+}
+
+window.onmouseup = () => {
+    if (!isPushing) return;
+
+    isPushing = false;
+    SetGPotential();
 }
 
 function SetPositions() {
-    sqrPos.x = square.getBoundingClientRect().x + sqrColRadius;
-    sqrPos.y = square.getBoundingClientRect().y + sqrColRadius;
+    groundY = container.getBoundingClientRect().bottom;
+}
 
-    groundY = window.innerHeight - container.getBoundingClientRect().bottom;
-} 
+function SetGPotential() {
+    var groundHeight = Math.max(0, groundY - sqrPos.y);
+    var velSqrd = 2 * gravity * (groundHeight - sqrHalfSize) + Math.pow(sqrVel.y, 2);
+    
+    gPotVel = Math.sqrt(Math.max(0, velSqrd * collisionDamping));
+}
 
-const VELOCITY_ERROR = 10;
-const gravity = 980; // value per frame ("cm")
-const groundDamping = 0.974; 
+function PushSquare() {
+    if (!isPushing) return;
 
-const MS_CONVERSOR = 0.001; // converts ms to seconds
-const FRAME_TIME = 16; // ms
+    return mPos.Subtract(sqrPos).Mult(velPerDist);
+}
 
-var deltaPos = 0;
-var deltaVel = 0;
+function DetectCollision() {
+    if (sqrPos.y + sqrHalfSize >= groundY) {
+        sqrVel.x *= (1 - collisionFriction);
+        sqrVel.y = -gPotVel;
+        
+        SetGPotential();
+    }
+}
 
-const process = async (frameTime) => {
+function MoveSquare(sqrPos, frameTime) {
+    square.animate({
+        transform: `translateX(${ sqrPos.x }px)
+                    translateY(${ sqrPos.y }px)`
+    }, {
+        duration: frameTime,
+        fill: "forwards"
+    });
+}
+
+async function Process(frameTime) {
     const deltaTime = frameTime * MS_CONVERSOR;
+    sqrAcc = new Vector(0, gravity);
 
     while (true) {
         await Sleep(frameTime);
         SetPositions();
         
-        deltaVel += gravity * deltaTime;
+        sqrVel = !PushSquare() ? sqrVel.Add(sqrAcc.Mult(deltaTime)) : PushSquare();
+        DetectCollision();
         
-        if (sqrPos.y - deltaVel * deltaTime <= groundY) deltaVel = Math.abs(deltaVel) * -groundDamping;
-        if (Math.abs(deltaVel) < VELOCITY_ERROR) deltaVel = 0;
+        if (sqrVel.MagSqrd() <= Math.pow(VELOCITY_ERROR, 2)) sqrVel = new Vector();
         
-        deltaPos += deltaVel * deltaTime;        
-        
-        square.animate({
-            transform: `translateY(${deltaPos}px)`
-        }, {
-            duration: FRAME_TIME,
-            fill: "forwards"
-        });
+        sqrPos = sqrPos.Add(sqrVel.Mult(deltaTime));
+        MoveSquare(sqrPos, FRAME_TIME);
     }
 }
 
-const WaitStart = async () => {
+function Start() {
+    sqrPos = startPosition;
+    
     SetPositions();
+    SetGPotential();
 
-    await Sleep(10000);
-    process(FRAME_TIME);
+    MoveSquare(sqrPos, 0);
+}
+
+async function Play() {
+    Start();
+
+    await Sleep(FRAME_TIME);
+    Process(FRAME_TIME);
 }
 
 // execution
 
-WaitStart();
+Play();
